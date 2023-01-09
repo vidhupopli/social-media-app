54832d96f913fb88fb3086fbc9189248fec50546
Vidhu Popli <vidhu278@gmail.com>
2023-01-09 14:09:54 +0530

    l32 add user registeration feature

--- app/components/home-guest.js
+++ app/components/home-guest.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Page from './page';
+import axios from 'axios';
 
 function HomeGuest() {
+  // states for input fields
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  //utility function to reset input fields states
+  const resetInputStates = () => {
+    setUsername('');
+    setEmail('');
+    setPassword('');
+  };
+
+  const userSignupHandler = async e => {
+    e.preventDefault();
+    try {
+      const userCredentialsForRegisteraion = { username, email, password };
+
+      await axios.post('http://localhost:8080/register', userCredentialsForRegisteraion);
+
+      resetInputStates();
+
+      alert('user created!');
+    } catch (err) {
+      console.log(err.response.data);
+    }
+  };
+
   return (
     <Page title="Home | SocialApp" narrow={false}>
       <div className="row align-items-center">
@@ -10,24 +38,24 @@ function HomeGuest() {
           <p className="lead text-muted">Are you sick of short tweets and impersonal &ldquo;shared&rdquo; posts that are reminiscent of the late 90&rsquo;s email forwards? We believe getting back to actually writing is the key to enjoying the internet again.</p>
         </div>
         <div className="col-lg-5 pl-lg-5 pb-3 py-lg-5">
-          <form>
+          <form onSubmit={userSignupHandler}>
             <div className="form-group">
               <label htmlFor="username-register" className="text-muted mb-1">
                 <small>Username</small>
               </label>
-              <input id="username-register" name="username" className="form-control" type="text" placeholder="Pick a username" autoComplete="off" />
+              <input onChange={e => setUsername(e.target.value)} value={username} id="username-register" name="username" className="form-control" type="text" placeholder="Pick a username" autoComplete="off" />
             </div>
             <div className="form-group">
               <label htmlFor="email-register" className="text-muted mb-1">
                 <small>Email</small>
               </label>
-              <input id="email-register" name="email" className="form-control" type="text" placeholder="you@example.com" autoComplete="off" />
+              <input onChange={e => setEmail(e.target.value)} value={email} id="email-register" name="email" className="form-control" type="text" placeholder="you@example.com" autoComplete="off" />
             </div>
             <div className="form-group">
               <label htmlFor="password-register" className="text-muted mb-1">
                 <small>Password</small>
               </label>
-              <input id="password-register" name="password" className="form-control" type="password" placeholder="Create a password" />
+              <input onChange={e => setPassword(e.target.value)} value={password} id="password-register" name="password" className="form-control" type="password" placeholder="Create a password" />
             </div>
             <button type="submit" className="py-3 mt-4 btn btn-lg btn-success btn-block">
               Sign up for SocialApp
