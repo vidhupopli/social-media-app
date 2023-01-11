b217769ccba97ec1e63e05480c04715d4bbb316d
Vidhu Popli <vidhu278@gmail.com>
2023-01-11 10:18:17 +0530

    add viewing individual posts

--- app/components/single-post.js
+++ app/components/single-post.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+
+// my components
 import Page from './page';
 
 function SinglePost() {
+  const { id } = useParams();
+
+  const [singlePostData, setSinglePostData] = useState(null);
+
+  useEffect(() => {
+    (async function () {
+      try {
+        const serverResponse = await axios.get(`/post/${id}`);
+
+        setSinglePostData(serverResponse.data);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, []);
+
+  if (!singlePostData) return <p>Loading...</p>;
+
   return (
-    <Page title="Fake Title" narrow={true}>
+    <Page title={singlePostData.title} narrow={true}>
       <div className="d-flex justify-content-between">
-        <h2>Example Post Title</h2>
+        <h2>{singlePostData.title}</h2>
         <span className="pt-2">
           <a href="#" className="text-primary mr-2" title="Edit">
             <i className="fas fa-edit"></i>
@@ -18,16 +40,13 @@ function SinglePost() {
 
       <p className="text-muted small mb-4">
         <a href="#">
-          <img className="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" />
+          <img className="avatar-tiny" src={singlePostData.author.avatar} />
         </a>
-        Posted by <a href="#">brad</a> on 2/10/2020
+        Posted by <a href="#">{singlePostData.author.username}</a> on {new Date(singlePostData.createdDate).toDateString()}
       </p>
 
       <div className="body-content">
-        <p>
-          Lorem ipsum dolor sit <strong>example</strong> post adipisicing elit. Iure ea at esse, tempore qui possimus soluta impedit natus voluptate, sapiente saepe modi est pariatur. Aut voluptatibus aspernatur fugiat asperiores at.
-        </p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quod asperiores corrupti omnis qui, placeat neque modi, dignissimos, ab exercitationem eligendi culpa explicabo nulla tempora rem? Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ea at esse, tempore qui possimus soluta impedit natus voluptate, sapiente saepe modi est pariatur. Aut voluptatibus aspernatur fugiat asperiores at.</p>
+        <p>{singlePostData.body}</p>
       </div>
     </Page>
   );
