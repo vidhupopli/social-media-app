6d721d32a37d63fa04b3bcb621bdce838997afb9
Vidhu Popli <vidhu278@gmail.com>
2023-01-23 11:04:57 +0530

    refactor useState into useImmer in the profile.js

--- app/components/profile.js
+++ app/components/profile.js
@@ -1,9 +1,10 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useImmer } from 'use-immer';
 import axios from 'axios';
 
 // my context
-import StateContext from '../contexts/state-context';
+import GlobalStateContext from '../contexts/state-context';
 
 // my components
 import Page from './page';
@@ -11,18 +12,21 @@ import ProfilePosts from './profile-posts';
 
 function Profile() {
   const { username } = useParams();
-  const stateRef = useContext(StateContext);
+  const globalState = useContext(GlobalStateContext);
 
-  const [profileData, setProfileData] = useState({
-    profileUsername: '...',
-    profileAvatar: '	https://gravatar.com/avatar/460c63a5312e0d71ab158836abb9d281?s=128',
-    isFollowing: false,
-    counts: {
-      postCount: '...',
-      followerCount: '...',
-      followingCount: '...'
+  const initialValue = {
+    profileData: {
+      profileUsername: '...',
+      profileAvatar: '	https://gravatar.com/avatar/460c63a5312e0d71ab158836abb9d281?s=128',
+      isFollowing: false,
+      counts: {
+        postCount: '...',
+        followerCount: '...',
+        followingCount: '...'
+      }
     }
-  });
+  };
+  const [localState, setLocalState] = useImmer(initialValue);
 
   // guideline: do not put the async function outside of the useEffect
 
@@ -32,9 +36,11 @@ function Profile() {
 
     (async function () {
       try {
-        const serverResponse = await axios.post(`/profile/${username}`, { token: stateRef?.userCredentials?.token }, { cancelToken: axiosRequestRef.token });
+        const serverResponse = await axios.post(`/profile/${username}`, { token: globalState?.userCredentials?.token }, { cancelToken: axiosRequestRef.token });
 
-        setProfileData(serverResponse.data);
+        setLocalState(curVal => {
+          curVal.profileData = serverResponse.data;
+        });
       } catch (err) {
         console.log(err);
       }
@@ -46,7 +52,7 @@ function Profile() {
   return (
     <Page title="My Posts" narrow={true}>
       <h2>
-        <img className="avatar-small" src={profileData.profileAvatar} /> {profileData.profileUsername}
+        <img className="avatar-small" src={localState.profileData.profileAvatar} /> {localState.profileData.profileUsername}
         <button className="btn btn-primary btn-sm ml-2">
           Follow <i className="fas fa-user-plus"></i>
         </button>
@@ -54,15 +60,16 @@ function Profile() {
 
       <div className="profile-nav nav nav-tabs pt-2 mb-4">
         <a href="#" className="active nav-item nav-link">
-          Posts: {profileData.counts.postCount}
+          Posts: {localState.profileData.counts.postCount}
         </a>
         <a href="#" className="nav-item nav-link">
-          Followers: {profileData.counts.followerCount}
+          Followers: {localState.profileData.counts.followerCount}
         </a>
         <a href="#" className="nav-item nav-link">
-          Following: {profileData.counts.followingCount}
+          Following: {localState.profileData.counts.followingCount}
         </a>
       </div>
+
       <ProfilePosts />
     </Page>
   );
